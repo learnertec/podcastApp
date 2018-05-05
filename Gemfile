5c646b1cfad93ffc65e2082305f4fbfc6b788a9b
Your Name <you@example.com>
2018-05-05 21:19:23 +0530

    added willpaginate& paperclip &

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'turbolinks'
 gem 'jbuilder', '~> 2.0'
 gem 'sdoc', '~> 0.4.0', group: :doc
 gem 'devise', '~>4.2'
+gem 'paperclip', '~> 4.2', '>= 4.2.1'
+gem 'aws-sdk', '~>2.0.33'
+gem 'will_paginate', '~>3.0.7'
 
 group :development, :test do
   gem 'byebug'
--- app/models/podcast.rb
+++ app/models/podcast.rb
@@ -5,4 +5,6 @@ class Podcast < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
          has_many :episodes
+     has_attached_file :thumbnail, :styles => { :large => "1000x1000#", :thumb => "550x550>" }
+  validates_attachment_content_type :thumbnail, :content_type => /\Aimage\/.*\Z/
 end
