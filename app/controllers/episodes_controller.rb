09b7026cd961e857f577878140f588a69522ce73
Your Name <you@example.com>
2018-05-05 17:12:00 +0530

    added authorization to podcast

--- app/controllers/episodes_controller.rb
+++ app/controllers/episodes_controller.rb
@@ -1,4 +1,6 @@
 class EpisodesController < ApplicationController
+  before_action :authenticate_podcast!,except: [:show]
+  before_filter :require_permission
   before_action :find_podcast
   before_action :find_episode, only: [:show,:edit,:update,:destroy]
  def new
@@ -47,4 +49,11 @@ class EpisodesController < ApplicationController
  def find_episode
  	@episode = Episode.find(params[:id])
  end
+
+ def require_permission
+  @podcast = Podcast.find(params[:podcast_id])
+  if current_podcast != @podcast
+    redirect_to root_path, notice: "You are not allowed to view that page"
+ end
+end
 end
